7da1ea60bf222ba96d9386baacc456cf5f601e71
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-12-01 18:58:45 +0200

    create auth views

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer, RegisterSerializer
 
-# Create your views here.
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User created successfully."
+        })
+
+# Login API
+class LoginAPI(TokenObtainPairView):
+    pass
+
+# Logout API
+class LogoutAPI(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logout successful."})
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
