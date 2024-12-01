f95ca94b689b7664d4ecd79eb904c3b47073490a
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-12-01 18:17:18 +0200

    added user serializers

--- crud/serializers.py
+++ crud/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import authors, books
+from django.contrib.auth.models import User
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,24 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = books
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'username', 'email')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
